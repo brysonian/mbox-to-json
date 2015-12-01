d0421e46b54c6566d1346cf0b8ac6b088d3dd9b6
Chandler McWilliams <chandler@brysonian.com>
2015-11-30 21:08:14 -0800

    First!

---
+++ README.md
@@ -0,0 +1,8 @@
+Easily convert mbox files to json.
+
+# USAGE
+1. install dependencies with `npm install`
+2. run with `./mbox-to-json.js --input FILENAME.mbox`
+
+You can dump the JSON to a file by specifying the --output option:
+`./mbox-to-json.js --input FILENAME.mbox --output FILENAME.json`
---
+++ mbox-to-json.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+var MailParser  = require('mailparser').MailParser;
+var fs					= require('fs');
+var Mbox        = require('node-mbox');
+var argv				= require('yargs')
+									.alias('i', 'input')
+									.alias('o', 'output')
+									.demand(['i'])
+									.argv;
+
+function main() {
+	var messages = [];
+	var total = Infinity;
+	var mbox = new Mbox();
+	mbox.on('message', function(msg) {
+	  // parse message using MailParser
+	  var mailparser = new MailParser({ streamAttachments : true });
+	  mailparser.on('end', function(mail) {
+	  	messages.push(mail);
+	  	if (messages.length == messageCount) {
+	  		console.log('Finished parsing messages');
+	  		if (argv.o)
+	  			saveAsJSON(messages);
+	  		else
+	  			console.log(messages);
+	  	}
+	  });
+	  mailparser.write(msg);
+	  mailparser.end();
+	});
+
+	mbox.on('end', function(parsedCount) {
+		console.log('Completed Parsing mbox File.');
+		messageCount = parsedCount;
+	});
+
+	if (fs.existsSync(argv.input)) {
+		var handle = fs.createReadStream(argv.input);
+		//handle.setEncoding('ascii');
+		handle.pipe(mbox);
+	}
+}
+
+function saveAsJSON(obj) {
+	fs.writeFile(argv.output, JSON.stringify(obj), function(err) {
+		if(err)
+			return console.log(err);
+	});
+}
+main();
